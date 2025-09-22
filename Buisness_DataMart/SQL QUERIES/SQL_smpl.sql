e8b196bde05f8d550642ca8c9e765fc4947b9279
amanmishra1888 <117447510+amanmishra1888@users.noreply.github.com>
2025-09-22 23:42:55 +0530

    Delete Buisness_DataMart directory

--- Buisness_DataMart/SQL QUERIES/SQL_smpl.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Customers (first_name, last_name, email) 
-VALUES ('Aman', 'Mishra', 'Aman.Mishra@example.com');
-
-Select * from Customers;
-Select * from products;
-Select * from orders;
